5ce3d6845f99e259f771333060ced4b255d754a3
kleberfreitas2 <48223529+kleberfreitas2@users.noreply.github.com>
2026-04-09 11:57:45 -0300

    Revert "Nova interface"
    This reverts commit 448e4a186d7579830563ebe09f673b693193c286.

--- GameLauncher/ViewModels/MainViewModel.cs
+++ GameLauncher/ViewModels/MainViewModel.cs
@@ -58,7 +58,6 @@ public partial class MainViewModel : ObservableObject, IDisposable
     [ObservableProperty] private string gamepadStatus = "";
 
     public bool HasBackgroundImage => BackgroundImage is not null;
-    public bool HasGames => Games.Count > 0;
 
     public ICollectionView GamesView => _gamesView;
 
@@ -67,7 +66,6 @@ public partial class MainViewModel : ObservableObject, IDisposable
         _dispatcher = Dispatcher.CurrentDispatcher;
 
         _gamesView = CollectionViewSource.GetDefaultView(Games);
-        Games.CollectionChanged += (_, _) => OnPropertyChanged(nameof(HasGames));
         _gamesView.Filter = obj =>
             obj is Game g &&
             (string.IsNullOrWhiteSpace(SearchText) ||
@@ -128,8 +126,6 @@ public partial class MainViewModel : ObservableObject, IDisposable
             foreach (var g in saved)
                 Games.Add(g);
             StatusMessage = $"{Games.Count} jogos na biblioteca";
-            if (Games.Count > 0)
-                SelectedGame = Games[0];
         }
         catch { }
     }
@@ -175,11 +171,7 @@ public partial class MainViewModel : ObservableObject, IDisposable
 
         SaveGames();
         StatusMessage = $"{Games.Count} jogos na biblioteca";
-        SelectedGame ??= Games.FirstOrDefault();
     }
-
-    [RelayCommand]
-    private void SelectGame(Game game) => SelectedGame = game;
 
     [RelayCommand]
     private void ChangeImage(Game game)
